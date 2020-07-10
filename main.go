e561c0f03f6e7cae8a27a3538d8559455f9ffef8
hellochan <284910889@qq.com>
2020-07-10 16:13:50 +0800

    init commit

--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+/**
+Version of Delve is too old for this version of Go
+Delve 是调试工具，由于版本过低，不能使用debug模式
+
+*/
 func main() {
 	fmt.Println("这是第一个go程序，向世界打个招呼吧，hello world")
 }
