67e01ade02acbbcfe219c35f5bc554739f96930e
hellochan <284910889@qq.com>
2020-07-10 17:08:15 +0800

    变量定义

--- main.go
+++ main.go
@@ -18,6 +18,9 @@ func main() {
 	age = 12
 	fmt.Println(name)
 	fmt.Println(age)
+	if success {
+		fmt.Println("成功进入")
+	}
 }
 
 const a string = "a"        // 常量 const 常量名称 数据类型 = 变量值
@@ -29,3 +32,34 @@ var (
 	name = "chen"
 	age  int
 )
+var success bool = false
+
+/*
+	算数运算符: + - * / % ++ --
+	关系运算符: == != > < >= <=
+	逻辑运算符: && || !
+	赋值运算符: = += -= *= /= %= <<= >>= &= |= 指定运算后赋值
+*/
+
+/*
+	if 布尔表达式 {} else {}
+	switch var {
+		case "":
+		case "":
+		case "":
+		default:
+	}
+	select {
+
+
+	}
+*/
+
+/*
+	for init; condition; post { }
+	for condition { }
+	for { }
+	init： 一般为赋值表达式，给控制变量赋初值；
+	condition： 关系表达式或逻辑表达式，循环控制条件；
+	post： 一般为赋值表达式，给控制变量增量或减量。
+*/
